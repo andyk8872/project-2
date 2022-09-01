5b5aab296d94c187970ae1937d5c2168a02a714b
andyk8872 <andrewkennedy35@yahoo.ie>
2022-09-01 17:05:55 +0000

    feat: Create function playGame

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,32 @@ console.log(userPicks);
 let playerDecide;
 
 for (var i = 0; i < userPicks.length; i++) {
-    document.querySelectorAll("button")[i].addEventListener("click", function(event) {
+    document.querySelectorAll("button")[i].addEventListener("click", function (event) {
 
-        playerDecide = event.target.id;        
-   
-        let userResult = "assets/images/" + playerDecide + ".png";
-        document.querySelectorAll("img")[0].setAttribute("src", userResult);
+        playerDecide = event.target.id;
 
-        let cRandom = computerPick();
-        let randomComp = "assets/images/" + cRandom + ".png";
-        document.querySelectorAll("img")[1].setAttribute("src", randomComp);
-   
-       });      
-   }  
+        // let userResult = "assets/images/" + playerDecide + ".png";
+        // document.querySelectorAll("img")[0].setAttribute("src", userResult);
 
-   function computerPick() {
+        // let cRandom = computerPick();
+        // let randomComp = "assets/images/" + cRandom + ".png";
+        // document.querySelectorAll("img")[1].setAttribute("src", randomComp);
+    playGame(playerDecide);
+    });
+}
+
+function playGame(playerDecide) {
+
+    let userResult = "assets/images/" + playerDecide + ".png";
+    document.querySelectorAll("img")[0].setAttribute("src", userResult);
+
+    let cRandom = computerPick();
+    let randomComp = "assets/images/" + cRandom + ".png";
+    document.querySelectorAll("img")[1].setAttribute("src", randomComp);
+}
+
+function computerPick() {
     let picks = ["rock", "paper", "scissors", "spock", "lizard"];
-    let randomPick = (Math.floor(Math.random() * 5));     
-    return picks[randomPick]; 
+    let randomPick = (Math.floor(Math.random() * 5));
+    return picks[randomPick];
 }
