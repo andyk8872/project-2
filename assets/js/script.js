754f13c36e806b8cbd925055ef7330b0031d5a2e
andyk8872 <andrewkennedy35@yahoo.ie>
2022-09-01 13:07:55 +0000

    Get 'playerChoice' using fol loop + addeventListener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,12 @@
 const userPicks = document.getElementsByTagName('button');
 console.log(userPicks);
+
+let playerDecide;
+
+for (var i = 0; i < userPicks.length; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function(event) {
+        playerDecide = event.target.id;
+        console.log(playerDecide);
+
+    });  
+}
