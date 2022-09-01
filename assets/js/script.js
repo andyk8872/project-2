d57b79eba8afe7e7274dac6bff86805fa07bf02e
andyk8872 <andrewkennedy35@yahoo.ie>
2022-09-01 21:54:36 +0000

    Add 2 global variables for results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 /* Global Variables */
 const userPicks = document.getElementsByTagName('button');
+const computerChoice = document.getElementById('computer-choice');
+const result = document.getElementById('result');
 
 let playerDecide;
+let decision;
 
 /** Record which button is clicked and 
  * run play functionGame */
@@ -10,11 +13,12 @@ for (var i = 0; i < userPicks.length; i++) {
 
         playerDecide = event.target.id;
         playGame(playerDecide);
-        console.log(playerDecide);
+        console.log(playerDecide);       
 
     });
 }
 
+/* A function to play the game */
 function playGame(playerDecide) {
 
     let userResult = "assets/images/" + playerDecide + ".png";
@@ -22,11 +26,12 @@ function playGame(playerDecide) {
 
     let cRandom = computerPick();
     let randomComp = "assets/images/" + cRandom + ".png";
-    document.querySelectorAll("img")[1].setAttribute("src", randomComp);
-}
+    document.querySelectorAll("img")[1].setAttribute("src", randomComp);    
+}   
 
 function computerPick() {
     let picks = ["rock", "paper", "scissors", "spock", "lizard"];
     let randomPick = (Math.floor(Math.random() * 5));
     return picks[randomPick];
 }
+
