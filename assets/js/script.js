badae56405725c6fb093d060da43217328be856b
andyk8872 <andrewkennedy35@yahoo.ie>
2022-09-01 13:22:40 +0000

    feat: Add new image when user click botton

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,12 @@ let playerDecide;
 
 for (var i = 0; i < userPicks.length; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function(event) {
+
         playerDecide = event.target.id;
         console.log(playerDecide);
-
-    });  
-}
+   
+        let userResult = "assets/images/" + playerDecide + ".png";
+        document.querySelectorAll("img")[0].setAttribute("src", userResult);
+   
+       });      
+   }  
