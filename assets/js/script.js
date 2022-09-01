0b69f97f5c369c2358fdc62263ccce0b30af3701
andyk8872 <andrewkennedy35@yahoo.ie>
2022-09-01 15:02:16 +0000

    feat: add function for random image selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,18 @@ let playerDecide;
 for (var i = 0; i < userPicks.length; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function(event) {
 
-        playerDecide = event.target.id;
-        console.log(playerDecide);
+        playerDecide = event.target.id;        
    
         let userResult = "assets/images/" + playerDecide + ".png";
         document.querySelectorAll("img")[0].setAttribute("src", userResult);
+
+        console.log(computerPick());
    
        });      
    }  
+
+   function computerPick() {
+    let picks = ["rock", "paper", "scissors", "spock", "lizard"];
+    let randomPick = (Math.floor(Math.random() * 5));     
+    return picks[randomPick]; 
+}
