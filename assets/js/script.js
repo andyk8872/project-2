33c43e6bdb5347721e1f2f1fc54c9a76f6cbaa1a
andyk8872 <andrewkennedy35@yahoo.ie>
2022-09-03 16:25:11 +0000

    feat: add 'instructions' in html and styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */ 
+
 /* Global Variables */
 const userPicks = document.getElementsByTagName('button');
 const computerChoice = document.getElementById('computer-choice');
@@ -13,7 +15,7 @@ let timeLeft = 5;
 
 /** Record which button is clicked and 
  * run play functionGame */
-for (var i = 0; i < userPicks.length; i++) {
+for (let i = 0; i < userPicks.length; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function (event) {
         let btnChoice = event.target.tagName;
 
@@ -38,8 +40,8 @@ function playGame(playerDecide) {
     getResult();
     increaseScore();
     final.innerHTML = endGame();
-   
-}   
+
+}
 
 /* Choose a ramdom game item/image */
 function computerPick() {
@@ -50,50 +52,50 @@ function computerPick() {
 
 /*Work out the result/winner and display */
 function getResult() {
-    let computer = computerChoice.textContent; 
-     
-    switch(true) {
+    let computer = computerChoice.textContent;
+
+    switch (true) {
         case computer === playerDecide:
-           decision = 'Draw';
-           break;
+            decision = 'Draw';
+            break;
         case playerDecide === 'scissors' && computer === 'paper':
-        case playerDecide === 'scissors' && computer === 'lizard': 
+        case playerDecide === 'scissors' && computer === 'lizard':
         case playerDecide === 'paper' && computer === 'rock':
-        case playerDecide === 'paper' && computer === 'spock': 
+        case playerDecide === 'paper' && computer === 'spock':
         case playerDecide === 'rock' && computer === 'lizard':
-        case playerDecide === 'rock' && computer === 'scissors': 
+        case playerDecide === 'rock' && computer === 'scissors':
         case playerDecide === 'lizard' && computer === 'spock':
-        case playerDecide === 'lizard' && computer=== 'paper':
+        case playerDecide === 'lizard' && computer === 'paper':
         case playerDecide === 'spock' && computer === 'rock':
-        case playerDecide === 'spock' && computer === 'scissors':        
-            decision = 'You Win!';          
+        case playerDecide === 'spock' && computer === 'scissors':
+            decision = 'You Win!';
             break;
         case playerDecide === 'paper' && computer === 'scissors':
-        case playerDecide === 'lizard' && computer=== 'scissors': 
-        case playerDecide === 'rock' && computer=== 'paper':
-        case playerDecide === 'spock' && computer === 'paper': 
+        case playerDecide === 'lizard' && computer === 'scissors':
+        case playerDecide === 'rock' && computer === 'paper':
+        case playerDecide === 'spock' && computer === 'paper':
         case playerDecide === 'lizard' && computer === 'rock':
-        case playerDecide === 'scissors' && computer === 'rock': 
+        case playerDecide === 'scissors' && computer === 'rock':
         case playerDecide === 'spock' && computer === 'lizard':
         case playerDecide === 'paper' && computer === 'lizard':
         case playerDecide === 'rock' && computer === 'spock':
-        case playerDecide === 'scissors' && computer === 'spock':   
+        case playerDecide === 'scissors' && computer === 'spock':
             decision = 'You Lose!';
             break;
         default:
             decision = "Something Is Wrong";
-    }    
-   
+    }
+
     if (decision === "You Win!") {
         result.innerHTML = `${playerDecide} beats ${computer}`;
     } else if (decision === "You Lose!") {
         result.innerHTML = `${computer} beats ${playerDecide}`;
-      } else {
+    } else {
         result.innerHTML = "Draw!";
-      }
+    }
 }
-/* Increase score function (Fron code-institute 'Love Maths' project)  */
-function increaseScore() {   
+/* Increase score function (From 'code-institute' 'Love Maths' project)  */
+function increaseScore() {
 
     if (decision === 'You Win!') {
         userScore = parseInt(document.getElementById('user-score').innerText);
@@ -101,86 +103,87 @@ function increaseScore() {
     } else if (decision === 'You Lose!') {
         computerScore = parseInt(document.getElementById('computer-score').innerText);
         document.getElementById('computer-score').innerText = ++computerScore;
-    }    
-} 
+    }
+}
 
 /** The modal contianers using w3schools.com  * 
  * get, open and close the modal containers  */
- const modal = document.getElementById("aboutModal");
- const win = document.getElementById("winModal");
- const lose = document.getElementById("loseModal");
+const modal = document.getElementById("aboutModal");
+const win = document.getElementById("winModal");
+const lose = document.getElementById("loseModal");
 
- const btn = document.getElementById("myBtn"); 
+const btn = document.getElementById("myBtn");
 
- const span = document.getElementsByClassName("close")[0]; 
- 
- btn.onclick = function() {
-   modal.style.display = "block";
- };
- 
- span.onclick = function() {
-   modal.style.display = "none";
- };
+const span = document.getElementsByClassName("close")[0];
 
- /* Sound switch function */
- function clickSound(click) {
-    switch (click) {                   
+btn.onclick = function () {
+    modal.style.display = "block";
+};
+
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+/* Sound switch function */
+function clickSound(click) {
+    switch (click) {
         case "BUTTON":
             let scissors = new Audio("assets/sounds/button.wav");
             scissors.play();
-            break;        
-    
-            case "win":
-                let win = new Audio("assets/sounds/win.wav");
-                win.play();
-                break;
-          
-              case "lose":
-                  let lose = new Audio("assets/sounds/lose.wav");
-                  lose.play();
-                  break;
+            break;
 
-        default: console.log(click);
-    
-      }
- }
+        case "win":
+            let win = new Audio("assets/sounds/win.wav");
+            win.play();
+            break;
 
- /* The endGame function - timed restart, disables buttons and win/lose display */
- function endGame() {
+        case "lose":
+            let lose = new Audio("assets/sounds/lose.wav");
+            lose.play();
+            break;
+
+        default:
+            console.log(click);
+
+    }
+}
+
+/* The endGame function - timed restart, disables buttons and win/lose display */
+function endGame() {
     if (userScore === 3) {
         const inputs = document.getElementsByTagName("button");
         for (const input of inputs) {
-        input.disabled = true;
+            input.disabled = true;
         }
         finalResult = "A great victory";
         clickSound("win");
         win.style.display = "block";
         setTimeout(countdown, 1000);
-        setTimeout(function(){
+        setTimeout(function () {
             window.location.reload();
-         }, 5000);
-    }  else if (computerScore === 3) {
+        }, 5000);
+    } else if (computerScore === 3) {
         const inputs = document.getElementsByTagName("button");
         for (const input of inputs) {
-        input.disabled = true;
+            input.disabled = true;
         }
         finalResult = "Computer Rules";
         clickSound("lose");
         lose.style.display = "block";
         setTimeout(countdown, 1000);
-        setTimeout(function(){
+        setTimeout(function () {
             window.location.reload();
-         }, 5000);
+        }, 5000);
     }
     return finalResult;
- }
+}
 
- /* Countdown function */
- function countdown() {
-	timeLeft--;
-	document.getElementById("win-timer").innerHTML = String( timeLeft );
-    document.getElementById("lose-timer").innerHTML = String( timeLeft );
-	if (timeLeft > 0) {
-		setTimeout(countdown, 1000);
-	}
-};
+/* Countdown function */
+function countdown() {
+    timeLeft--;
+    document.getElementById("win-timer").innerHTML = String(timeLeft);
+    document.getElementById("lose-timer").innerHTML = String(timeLeft);
+    if (timeLeft > 0) {
+        setTimeout(countdown, 1000);
+    }
+}
